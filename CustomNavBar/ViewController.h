7aae45c22c114af690e37a567fa59e5808a1e51d
baozejian <baozejian@baozejiandeMacBook-Pro.local>
2015-07-02 22:04:56 +0800

    Initial Commit

---
+++ CustomNavBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomNavBar
+//
+//  Created by baozejian on 15/7/2.
+//  Copyright (c) 2015年 baozejian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomNavBar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomNavBar
+//
+//  Created by baozejian on 15/7/2.
+//  Copyright (c) 2015年 baozejian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
